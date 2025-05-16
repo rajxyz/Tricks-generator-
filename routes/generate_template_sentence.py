4ef2dd9f952b869074631d4a1d01b98e811be46e
rajxyz <atptricks@gmail.com>
2025-05-16 12:35:12 +0530

    Update generate_template_sentence.py

--- routes/generate_template_sentence.py
+++ routes/generate_template_sentence.py
@@ -1,3 +1,5 @@
+import random
+
 def generate_template_sentence(wordbank, templates, letters=None):
     if not templates:
         return "No templates available."
@@ -15,10 +17,10 @@ def generate_template_sentence(wordbank, templates, letters=None):
         sentence_template = random.choice(templates)
 
     return sentence_template.format(
-        Article=random.choice(wordbank.get("Articles", [""])),
-        Adjective=random.choice(wordbank.get("adjectives", [""])),
-        Noun=random.choice(wordbank.get("nouns", [""])),
-        Verb=random.choice(wordbank.get("verbs", [""])),
-        Adverb=random.choice(wordbank.get("adverbs", [""])),
-        Preposition=random.choice(wordbank.get("prepositions", [""]))
+        article=random.choice(wordbank.get("articles", [""])),
+        adjective=random.choice(wordbank.get("adjectives", [""])),
+        noun=random.choice(wordbank.get("nouns", [""])),
+        verb=random.choice(wordbank.get("verbs", [""])),
+        adverb=random.choice(wordbank.get("adverbs", [""])),
+        preposition=random.choice(wordbank.get("prepositions", [""]))
     )
