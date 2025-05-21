28da94857804dac6746e771fbb40f934c67bc95d
rajxyz <atptricks@gmail.com>
2025-05-21 12:43:49 +0530

    Update wiki.py

--- routes/wiki.py
+++ routes/wiki.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from typing import List
 import json
 from pathlib import Path
-from utils.wiki_utils import fetch_wikipedia_summary
+from routes.utils.wiki_utils import fetch_wikipedia_summary
 
 router = APIRouter()
 # rest of the code remains same
